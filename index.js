6ae3affeb17fcde012c56edbf74e7ecab1d9e11e
Taranjeet Singh <singhtaran1515@gmail.com>
2020-08-31 16:43:14 +0530

    Created Server for hitting API using Node

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ const server = http.createServer((req,res)=>{
         })      
     }
 
+    if(req.url === '/api/about/'){
+        
+        const user = [
+            { name: 'Taran', age:'22'},
+            { name: 'Taranjeet', age:'22'}
+        ]
+
+        res.writeHead(200, {'Content-Type':'application/json'});
+        res.end(JSON.stringify(user))
+
+    }
+
 
 });
 
